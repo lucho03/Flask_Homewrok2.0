70edfd3840745fc78ac88c239d98784ba0189f41
lucho03 <37477952+lucho03@users.noreply.github.com>
2021-01-27 10:58:46 +0200

    Create database.py for separate the code

---
+++ database.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from main import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    image = db.Column(db.String(20), nullable=False, default='image.jpg')
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.image}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}, {self.date}')"
