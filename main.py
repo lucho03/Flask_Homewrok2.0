549816290ec1ffa1cc3734592c9686af78031896
lucho03 <37477952+lucho03@users.noreply.github.com>
2021-01-27 16:31:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import Registration, Login
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '812bdubdqwpojdoiqd'
+app.config['SECRET_KEY'] = '0\xa5\x87-\xad\xa2z\x10d\xaf\x9br\xf8\x9f\x0c\xcbt\xe5\x83\x98'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from database import User, Post
 
@@ -31,8 +33,12 @@ def about():
 def regiter():
     form = Registration()
     if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f'{form.username.data} is  created susccessfully!', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
