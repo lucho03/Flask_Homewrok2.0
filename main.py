990eeac8e1c9818be37e982b524eb57e7dd7923f
lucho03 <37477952+lucho03@users.noreply.github.com>
2021-01-27 19:31:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'p\xee\xeb>\x077\xef\x0e\x87P\xbe\xbcV\xcaVv\x90\xc2\xe8\x1eB*C\xf5'
@@ -47,5 +47,10 @@ def regiter():
 def login():
     form = Login()
     if form.validate_on_submit():
-        flash('Unsuccessfull login!', 'danger')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('Unsuccessfull login!', 'danger')
     return render_template('login.html', form=form)
